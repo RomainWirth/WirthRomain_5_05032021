2970c89fe6e958033f20e84b94445e8fa9b985a0
RomainWirth <wirth.romain@gmail.com>
2021-06-25 12:00:08 +0200

    init index.json

---
+++ index.js
@@ -0,0 +1,13 @@
+fetch("http://localhost:3000/api/teddies")
+    .then (function (res) {
+        if (res.ok) {
+            return res.json();
+        }
+    })
+    .then (function (/*value*/) {
+        console.log (/*value*/);
+    })
+    .catch (function (err) {
+        // une erreur est survenue
+    });
+
