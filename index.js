3ed69da3d76ce1b3c7ae23cd7df8258f86f478d6
RomainWirth <wirth.romain@gmail.com>
2021-07-02 12:22:56 +0200

    préparation mise en oeuvre index.js

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ fetch("http://localhost:3000/api/teddies")
     .then (function(res) {
         console.log(res);
         
+        // récupérer les éléments de teddiesData dans un tableau
+
+        // générer les éléments HTML pour un item[i] : 
+            // div principale : <div class="catalog__item">
+            // photo : <div class="catalog__item--image_dimension", <a href> et <img>
+            // nom et prix : <div class="catalog__item--description", <a href> et <h2>, <a href> et <p>
+            // bouton buy-now : <div class="catalog__item--buy_now", <a href> et <p>
+        
+        // => intégrer les éléments dans la div principale
+
+        // relier les éléments du tableau dans les divs correspondantes pour item[i]
+
+        // dupliquer autant de fois la <div class"catalog__item"> qu'il y a d'élément parent dans le tableau
 
 
     })
