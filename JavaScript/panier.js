92e95b533d86861df738fc1d00c7ff8a686371a8
RomainWirth <wirth.romain@gmail.com>
2021-07-04 21:51:42 +0200

    modif panier.html et début rédac panier.js

--- JavaScript/panier.js
+++ JavaScript/panier.js
@@ -11,7 +11,24 @@ fetch("http://localhost:3000/api/teddies")
     .then(function(selectedItemData){
         console.log(selectedItemData);
 
-        
+
+
+
+
+        let newSelectedItemContainer = getElementById("Selection_Item_Container");
+        let newSelectedItemImgCont = document.createElement("div");
+        newSelectedItemImgCont.className = "selection__item--image-mini";
+        let newSelectedItemTitle = document.createElement("h3");
+        newSelectedItemTitle.className = "selection__item--title";
+        let newSelectedItemRef = document.createElement("p");
+        newSelectedItemRef.className = "selection__item--ref";
+        let newSelectedItemPrice = document.createElement("p");
+        newSelectedItemPrice.className = "selection__item--price";
+
+        newSelectedItemContainer.appendChild(newSelectedItemImgCont);
+        newSelectedItemContainer.appendChild(newSelectedItemTitle);
+        newSelectedItemContainer.appendChild(newSelectedItemRef);
+        newSelectedItemContainer.appendChild(newSelectedItemPrice);
 
 
     })
