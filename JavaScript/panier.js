c82066d36181db9a33e60ae9654abe07d796cfce
RomainWirth <wirth.romain@gmail.com>
2021-07-06 19:15:53 +0200

    fonction calcul prix total panier

--- JavaScript/panier.js
+++ JavaScript/panier.js
@@ -1,4 +1,6 @@
-
+// -----------------------------------------------------------------------------------------------------------------------------------------
+// ------------ Start Fonction -------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------------------------
 
 fetch("http://localhost:3000/api/teddies")
     .then(function(res) {
@@ -38,93 +40,63 @@ fetch("http://localhost:3000/api/teddies")
             for (m = 0; m < localStorageRegisteredItem.length; m++) {
                 console.log(localStorageRegisteredItem.length);
 
-        // création des éléments enfants et insertion des données stockées dans le localStorage
-            let newSelectedItemContainer = document.createElement("div");
-            newSelectedItemContainer.className = "selection__item";
+// création des éléments enfants et insertion des données stockées dans le localStorage
+                let newSelectedItemContainer = document.createElement("div");
+                newSelectedItemContainer.className = "selection__item";
     
-            let newSelectedItemImgCont = document.createElement("div");
-            newSelectedItemImgCont.className = "selection__item--image-mini";
-            let newSelectedItemImgUrl = document.createElement("img");
-            newSelectedItemImgUrl.className = "basket_item_image";
-            newSelectedItemImgUrl.setAttribute("src", localStorageRegisteredItem[m].itemImageUrl);
-            newSelectedItemImgCont.appendChild(newSelectedItemImgUrl);
+                let newSelectedItemImgCont = document.createElement("div");
+                newSelectedItemImgCont.className = "selection__item--image-mini";
+                let newSelectedItemImgUrl = document.createElement("img");
+                newSelectedItemImgUrl.className = "basket_item_image";
+                newSelectedItemImgUrl.setAttribute("src", localStorageRegisteredItem[m].itemImageUrl);
+                newSelectedItemImgCont.appendChild(newSelectedItemImgUrl);
     
-            let newSelectedItemTitle = document.createElement("h3");
-            newSelectedItemTitle.className = "selection__item--title";
-            newSelectedItemTitleTextNode = document.createTextNode(localStorageRegisteredItem[m].itemName);
-            newSelectedItemTitle.appendChild(newSelectedItemTitleTextNode);
+                let newSelectedItemTitle = document.createElement("h3");
+                newSelectedItemTitle.className = "selection__item--title";
+                newSelectedItemTitleTextNode = document.createTextNode(localStorageRegisteredItem[m].itemName);
+                newSelectedItemTitle.appendChild(newSelectedItemTitleTextNode);
     
-            let newSelectedItemRef = document.createElement("p");
-            newSelectedItemRef.className = "selection__item--ref";
-            let newSelectedItemRefTextNode = document.createTextNode(localStorageRegisteredItem[m].itemId);
-            newSelectedItemRef.appendChild(newSelectedItemRefTextNode);
+                let newSelectedItemRef = document.createElement("p");
+                newSelectedItemRef.className = "selection__item--ref";
+                let newSelectedItemRefTextNode = document.createTextNode(localStorageRegisteredItem[m].itemId);
+                newSelectedItemRef.appendChild(newSelectedItemRefTextNode);
     
-            let newSelectedItemOption = document.createElement("p");
-            newSelectedItemOption.className = "selection__item--option";
-            let newSelectedItemOptionTextNode = document.createTextNode(localStorageRegisteredItem[m].itemOption);
-            newSelectedItemOption.appendChild(newSelectedItemOptionTextNode);
+                let newSelectedItemOption = document.createElement("p");
+                newSelectedItemOption.className = "selection__item--option";
+                let newSelectedItemOptionTextNode = document.createTextNode(localStorageRegisteredItem[m].itemOption);
+                newSelectedItemOption.appendChild(newSelectedItemOptionTextNode);
     
-            let newSelectedItemPrice = document.createElement("p");
-            newSelectedItemPrice.className = "selection__item--price";
-            let newSelectedItemPriceTextNode = document.createTextNode(localStorageRegisteredItem[m].itemPrice + " Euros");
-            newSelectedItemPrice.appendChild(newSelectedItemPriceTextNode);
-                
-            let newSelectedItemSuppressButton = document.createElement("button");
-            newSelectedItemSuppressButton.setAttribute("type", "button");
-            newSelectedItemSuppressButton.className = "selection__item--suppress";
-            let newSelectedItemSuppressButtonTextNode = document.createTextNode("Supprimer");
-            newSelectedItemSuppressButton.appendChild(newSelectedItemSuppressButtonTextNode);
-                        
+                let newSelectedItemPrice = document.createElement("p");
+                newSelectedItemPrice.className = "selection__item--price";
+                let newSelectedItemPriceTextNode = document.createTextNode(localStorageRegisteredItem[m].itemPrice + " Euros");
+                newSelectedItemPrice.appendChild(newSelectedItemPriceTextNode);
+
+                let newSelectedItemSuppressButton = document.createElement("button");
+                newSelectedItemSuppressButton.setAttribute("type", "button");
+                newSelectedItemSuppressButton.className = "selection__item--suppress";
+                let newSelectedItemSuppressButtonTextNode = document.createTextNode("Supprimer");
+                newSelectedItemSuppressButton.appendChild(newSelectedItemSuppressButtonTextNode);
+
             // ajout des éléments enfants dans l'élément parent
-            newSelectedItemMainContainer.appendChild(newSelectedItemContainer);
-            newSelectedItemContainer.appendChild(newSelectedItemImgCont);
-            newSelectedItemContainer.appendChild(newSelectedItemTitle);
-            newSelectedItemContainer.appendChild(newSelectedItemRef);
-            newSelectedItemContainer.appendChild(newSelectedItemOption);
-            newSelectedItemContainer.appendChild(newSelectedItemPrice);
-            newSelectedItemContainer.appendChild(newSelectedItemSuppressButton);
-                
+                newSelectedItemMainContainer.appendChild(newSelectedItemContainer);
+                newSelectedItemContainer.appendChild(newSelectedItemImgCont);
+                newSelectedItemContainer.appendChild(newSelectedItemTitle);
+                newSelectedItemContainer.appendChild(newSelectedItemRef);
+                newSelectedItemContainer.appendChild(newSelectedItemOption);
+                newSelectedItemContainer.appendChild(newSelectedItemPrice);
+                newSelectedItemContainer.appendChild(newSelectedItemSuppressButton);
+
+    // cette fonction ne fonctionne pas correctement : à retravailler ======================================================================================           
             // fonction de suppression des éléments de la ligne
-            newSelectedItemSuppressButton.addEventListener("click", function(){
-                for (n = 0; n < localStorageRegisteredItem.length; n++) {
-                    var elementSuppress = localStorageRegisteredItem.splice(n, 1);
-                    console.log(elementSuppress);
-                    localStorage.setItem("product", JSON.stringify(localStorageRegisteredItem));
-                    window.location.href = "panier.html";
-                }
-            })
-            
+                newSelectedItemSuppressButton.addEventListener("click", function(){
+                    for (n = 0; n < localStorageRegisteredItem.length; n++) {
+                        var elementSuppress = localStorageRegisteredItem.splice(n, 1);
+                        console.log(elementSuppress);
+                        localStorage.setItem("product", JSON.stringify(localStorageRegisteredItem));
+                        window.location.href = "panier.html";
+                    }
+                })            
             }
-
-
-
-            /*
-            // sélection des ref de tous les boutons de suppression de ligne
-            newSelectedItemSuppressButton = document.querySelectorAll(".selection__item--suppress");
-            console.log(newSelectedItemSuppressButton); // affichage du bouton dans la console en tant que NodeList
-
-            for(let n = 0; n <= newSelectedItemSuppressButton.length; n++){
-                newSelectedItemSuppressButton[n].addEventListener("click", function(event) {
-                    event.preventDefault();
-                    console.log(event); 
-
-                    // selection de l'id de l'item qui sera supprimé
-                    let id_select_suppress = localStorageRegisteredItem[n].itemId;
-                    console.log(id_select_suppress);
-
-                    // supprimer l'objet du tableau avec la méthode filter : sélection des élts à conserver et ceux ou le btn suppress a été cliqué
-                    localStorageRegisteredItem = localStorageRegisteredItem.filter((element) => element.itemId !== id_select_suppress);
-                    console.log(localStorageRegisteredItem);
-
-                    // envoyer la variable dans le local storage
-                    localStorage.setItem("product", JSON.stringify(localStorageRegisteredItem));
-
-                    // message d'alerte de suppression du produit + rechargement de la page
-                    alert(`le produit ${localStorageRegisteredItem[n].itemName} a été supprimé du panier`);
-                    window.location.href = "panier.html";
-
-                })
-            } */
         
             // ajout du total du panier 
             let newSelectedItemTotalPriceCont = document.createElement("div");
@@ -134,8 +106,28 @@ fetch("http://localhost:3000/api/teddies")
             newSelectedItemTotal.appendChild(newSelectedItemTotalTextNode);
             newSelectedItemTotalPriceCont.appendChild(newSelectedItemTotal);
             let newSelectedItemTotalPrice = document.createElement("p");
+            newSelectedItemTotalPriceCont.appendChild(newSelectedItemTotalPrice);
             //injecter fonction calculant le prix total du panier
+            // création d'une variable avec un array vide 
+            let totalPriceArray = [];
 
+            for (let k = 0; k < localStorageRegisteredItem.length; k++) {
+                let itemPriceInBasket = localStorageRegisteredItem[k].itemPrice;
+                let itemPriceInBasketNumber = parseFloat(itemPriceInBasket); // conversion string en number
+                console.log(itemPriceInBasket); // contrôle des prix des items dans la console
+
+                totalPriceArray.push(itemPriceInBasketNumber);
+                console.log(totalPriceArray); // contrôle des informations du tableau
+            }
+
+            let reducer = (accumulator, currentValue) => accumulator + currentValue;
+            let totalPrice = totalPriceArray.reduce(reducer, 0);
+            let totalPriceShown = totalPrice.toFixed(2);
+            console.log(totalPrice); // contrôle du calcul
+
+
+            let newSelectedItemTotalPriceTextNode = document.createTextNode(totalPriceShown + " Euros");
+            newSelectedItemTotalPrice.appendChild(newSelectedItemTotalPriceTextNode);
 
             // bouton pour vider entièrement le panier
             let newSelectedItemTotalPriceSuppressButton = document.createElement("button");
@@ -159,3 +151,7 @@ fetch("http://localhost:3000/api/teddies")
     .catch (function(error) {
         console.log('Il y a eu un problème avec l\'opération fetch : ' + error.message)
     });
+
+// -----------------------------------------------------------------------------------------------------------------------------------------
+// -------------- End Fonction -------------------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------------------------
