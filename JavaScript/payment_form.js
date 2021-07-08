1e3621da38c0b2fab998e2c53e408b9f66fc9845
RomainWirth <wirth.romain@gmail.com>
2021-07-08 16:30:17 +0200

    travail sur payment form

--- JavaScript/payment_form.js
+++ JavaScript/payment_form.js
@@ -9,7 +9,9 @@ const displayHtmlForm = function() {
             <div class="payment_form__registration--container">
                 <h3>Votre identité</h3>
                 <label for=nom>Nom</label>
-                <input id=nom name=nom type=text placeholder="Prénom et NOM" required autofocus>
+                <input id=nom name=nom type=text placeholder="NOM" required autofocus>
+                <label for=prenom>Prénom</label>
+                <input id=prenom name=prenom type=text placeholder="Prénom" required>
                 <label for=email>Email</label>
                 <input id=email name=email type=email placeholder="exemple@domaine.com" required>
                 <label for=telephone>Téléphone</label>
@@ -19,9 +21,11 @@ const displayHtmlForm = function() {
                 <h3>Adresse de livraison</h3>
                 <label for=adresse>Adresse</label>
                 <input id=adresse name=adresse rows=5 required></textarea>
-                <label for=codepostal>Code postal</label>
-                <input id=codepostal name=codepostal type=text required>
-                <label for=pays>Pays</label>
+                <label for=codePostal>Code postal</label>
+                <input id=codePostal name=codePostal type=text required>
+                <label for=ville>VILLE</label>
+                <input id=ville name=ville type=text required>
+                <label for=pays>PAYS</label>
                 <input id=pays name=pays type=text required>
             </div>
             <div class="payment_form__registration--container">
@@ -43,12 +47,12 @@ const displayHtmlForm = function() {
                 <input id=nom_porteur name=nom_porteur type=text placeholder="Même nom que sur la carte" required>
             </div>
             <div class="payment_form__registration--container">
-                <a href="confirmation.html">
+                <a href="#" id="payment_form_button">
                     <p class="payment-button">&#32;Payer&#32;</p>
                 </a>
             </div>
         </form>
-    `;
+    `; // ajouter le lien sur le bouton de paiement
 
     // injection HTML
     insertForm.insertAdjacentHTML("afterbegin", formStructure);
@@ -56,3 +60,45 @@ const displayHtmlForm = function() {
 
 // afficher le formulaire 
 displayHtmlForm();
+
+// sélection du bouton de paiement du formulaire
+const paymentFormButton = document.getElementById("payment_form_button");
+console.log(paymentFormButton); // vérification de la sélection du bouton dans la console
+
+// eventListener sur le bouton de confirmation de paiement
+paymentFormButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    // récupération des valeurs du formulaire pour les mettre dans le local storage
+    localStorage.setItem("nom", document.querySelector("#nom").value);
+    localStorage.setItem("prenom", document.querySelector("#prenom").value);
+    localStorage.setItem("email", document.querySelector("#email").value);
+    localStorage.setItem("telephone", document.querySelector("#telephone").value);
+    localStorage.setItem("adresse", document.querySelector("#adresse").value);
+    localStorage.setItem("codePostal", document.querySelector("#codePostal").value);
+    localStorage.setItem("ville", document.querySelector("#ville").value);
+    localStorage.setItem("pays", document.querySelector("#pays").value);
+
+    const form = {
+        nom: localStorage.getItem("nom"),
+        prenom: localStorage.getItem("prenom"),
+        email: localStorage.getItem("email"),
+        telephone: localStorage.getItem("telephone"),
+        adresse: localStorage.getItem("adresse"),
+        codePostal: localStorage.getItem("codePostal"),
+        ville: localStorage.getItem("ville"),
+        pays: localStorage.getItem("pays")
+    }
+    console.log("form");
+    console.log(form); // console log du formulaire
+
+    // ajout des values du formulaire et les produits sélectionnés dans un objet à envoyer au serveur
+    
+    let localStorageRegisteredItem = JSON.parse(localStorage.getItem("product"));
+    const toSend = {
+        localStorageRegisteredItem,
+        form
+    }
+    console.log(toSend);
+
+})
+
